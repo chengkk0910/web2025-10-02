9f5ef64dbf4a79d693a1a9233fe592cab1b9f2c2
kai <cgk0910@gmail.com>
2025-12-04 09:14:23 +0800

    array vs object

---
+++ s20251204/assocArr.php
@@ -0,0 +1,52 @@
+<?php
+$students = [
+    [
+        'id' => 1,
+        'name' => 'amy',
+        'mobile' => '0911',
+    ],
+    [
+        'id' => 2,
+        'name' => 'bob',
+        'mobile' => '0922',
+    ],
+    [
+        'id' => 3,
+        'name' => 'cat',
+        'mobile' => '0933',
+    ],
+];
+
+
+$shops = [
+    [
+        'id' => 1,
+        'name' => '台北店',
+        'address' => '台北市…..',
+        'mobiles' => [
+            'iphone15' => 5,
+            'iphone16' => 3,
+            'iphone17' => 2,
+        ]
+    ],
+    [
+        'id' => 2,
+        'name' => '台中店',
+        'address' => '台中市….',
+        'mobiles' => [
+            'iphone15' => 4,
+            'iphone16' => 32,
+            'iphone17' => 2,
+        ]
+    ],
+    [
+        'id' => 3,
+        'name' => '高雄店',
+        'address' => '高雄市…..',
+        'mobiles' => [
+            'iphone15' => 0,
+            'iphone16' => 2,
+            'iphone17' => 1,
+        ]
+    ]
+];
---
+++ s20251204/indexArr.php
@@ -0,0 +1,8 @@
+<?php
+$indexArr = [1, 2, 3, 4, 5];
+
+$assocArr = [
+    "name" => "John",
+    "age" => 30,
+    "city" => "New York"
+];
