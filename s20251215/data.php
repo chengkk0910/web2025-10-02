7ffd0e88c1289047f9c442082cb1ab2d18512332
kai <cgk0910@gmail.com>
2025-12-15 13:52:01 +0800

    php js json

---
+++ s20251215/data.php
@@ -0,0 +1,25 @@
+<?php
+$myArr = [1, 2, 3, 4, 5];
+$assocArr = [
+    "name" => "John",
+    "age" => 30,
+    "city" => "New York"
+];
+
+$data = [
+    [
+        'id' => 1,
+        'name' => 'amy',
+        'mobile' => '0911'
+    ],
+    [
+        'id' => 2,
+        'name' => 'bob',
+        'mobile' => '0922'
+    ],
+    [
+        'id' => 3,
+        'name' => 'cat',
+        'mobile' => '0933'
+    ],
+];
